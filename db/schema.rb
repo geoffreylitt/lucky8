e77925f6d627cd95834c04beccd8bb3a749b70da
Geoffrey Litt <glitt@panoramaed.com>
2015-08-22 18:04:46 -0400

    add Mass Org ID

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822195935) do
+ActiveRecord::Schema.define(version: 20150822220338) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20150822195935) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "org_id"
   end
 
   create_table "schools_users", id: false, force: true do |t|
