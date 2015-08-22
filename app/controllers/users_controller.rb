1b5e02f40dafbcb0d917b56b6a7d9f8636611d0a
Geoffrey Litt <glitt@panoramaed.com>
2015-08-22 15:28:32 -0400

    add admin users who can see all lucky8s

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  # Only admins can see the full list of users
   def index
+    redirect_to :back, alert: "Access denied." unless current_user.admin?
     @users = User.all
   end
 
+  # Normal users can view their own show page (which is their Lucky 8 list).
+  # Admin users can view any user's show page.
   def show
     @user = User.find(params[:id])
-    unless @user == current_user
+    if !current_user.admin? && @user != current_user
       redirect_to :back, :alert => "Access denied."
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822190918) do
+ActiveRecord::Schema.define(version: 20150822192103) do
 
   create_table "schools", force: true do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150822190918) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
