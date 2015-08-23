c78d662eaf0f13f96ce1c3a68b7b6c164e53326d
Sagar Jauhari <sagarjauhari@gmail.com>
2015-08-22 21:46:09 -0400

    added map to show

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -13,5 +13,12 @@ class SchoolsController < ApplicationController
 
   def show
     @school = School.find(params[:id])
+
+    @schools_with_location = School.where(id: @school.id).where.not(latitude: nil)
+
+    gon.geocoded_hash = Gmaps4rails.build_markers(@schools_with_location) do |school, marker|
+      marker.lat school.latitude
+      marker.lng school.longitude
+    end
   end
 end
