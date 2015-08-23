3cc3fd30225c48cc963cf1db2d7621da96d1e8ad
Geoffrey Litt <glitt@panoramaed.com>
2015-08-22 21:35:11 -0400

    scrape race/ethnicity

--- lib/data_scraper.rb
+++ lib/data_scraper.rb
@@ -13,7 +13,7 @@ class DataScraper
     scrape_general
     scrape_grad_rates
     scrape_postgrad_intentions
-    scrape_school_size
+    scrape_school_size_and_race
 
     @school.save!
   end
@@ -38,13 +38,16 @@ class DataScraper
   def scrape_grad_rates
     doc = Nokogiri::HTML(open("http://profiles.doe.mass.edu/grad/grad_report.aspx?orgcode=#{@id}&orgtypecode=6&"))
 
-    grad_rates_tr = doc.css("td").select{ |td| td.text == "4-Year Graduation Rate  (2014)" }.first.parent.parent.css("tr.ccc")
-    rates = grad_rates_tr.css("td").map{ |td| td.text.strip.to_f }
+    grad_rates_tr_header = doc.css("td").select{ |td| td.text == "4-Year Graduation Rate  (2014)" }
+    if grad_rates_tr_header.present?
+      grad_rates_tr = grad_rates_tr_header.first.parent.parent.css("tr.ccc")
+      rates = grad_rates_tr.css("td").map{ |td| td.text.strip.to_f }
 
-    set_field_if_nil(:four_yr_graduated, rates[2])
-    set_field_if_nil(:four_yr_still_in_school, rates[3])
-    set_field_if_nil(:four_yr_ged, rates[5])
-    set_field_if_nil(:four_yr_dropped_out, rates[6])
+      set_field_if_nil(:four_yr_graduated, rates[2])
+      set_field_if_nil(:four_yr_still_in_school, rates[3])
+      set_field_if_nil(:four_yr_ged, rates[5])
+      set_field_if_nil(:four_yr_dropped_out, rates[6])
+    end
   end
 
   def scrape_postgrad_intentions
@@ -72,30 +75,43 @@ class DataScraper
 
     # Scrape school size by grade
 
-    enrollment_table = doc.css("th").select do |th|
+    enrollment_table_header = doc.css("th").select do |th|
       th.text.strip.include? "Enrollment by Grade"
-    end.first.parent.parent.parent
+    end
 
-    nums = enrollment_table.css("tr").last.css("td").map{ |td| td.text.strip }
+    if enrollment_table_header.present?
+      enrollment_table = enrollment_table_header.first.parent.parent.parent
 
-    set_field_if_nil(:school_size_9_grade, nums[11].to_i)
-    set_field_if_nil(:school_size_10_grade, nums[12].to_i)
-    set_field_if_nil(:school_size_11_grade, nums[13].to_i)
-    set_field_if_nil(:school_size_12_grade, nums[14].to_i)
-    set_field_if_nil(:school_size_unknown, nums[16].to_i)
+      nums = enrollment_table.css("tr").last.css("td").map{ |td| td.text.strip }
+
+      set_field_if_nil(:school_size_9_grade, nums[11].to_i)
+      set_field_if_nil(:school_size_10_grade, nums[12].to_i)
+      set_field_if_nil(:school_size_11_grade, nums[13].to_i)
+      set_field_if_nil(:school_size_12_grade, nums[14].to_i)
+      set_field_if_nil(:school_size_unknown, nums[16].to_i)
+    end
 
     # Scrape demographic breakdown
 
-    race_table = doc.css("th").select do |th|
+    race_table_header = doc.css("th").select do |th|
       th.text.strip.include? "Race/Ethnicity"
-    end.first.parent.parent.parent
-
-    race_table.css("tr").each do |tr|
-      case tr.css("td").first.text.strip
-      when "African American"
-        set_field_if_nil(:school_population_african_american, tr.css("td").second.text.strip)
     end
 
+    if race_table_header.present?
+      race_table = race_table_header.first.parent.parent
+
+      race_percentages = race_table.css("tr").each_with_object({}) do |tr, hsh|
+        if tr.css("td").length > 2
+          hsh[tr.css("td").first.text.strip] = tr.css("td")[1].text.strip.to_f
+        end
+      end
+
+      set_field_if_nil(:school_population_african_american, race_percentages["African American"])
+      set_field_if_nil(:school_population_asian, race_percentages["Asian"])
+      set_field_if_nil(:school_population_hispanic, race_percentages["Hispanic"])
+      set_field_if_nil(:school_population_white, race_percentages["White"])
+      set_field_if_nil(:school_population_multi_race_non_hispanic, race_percentages["Multi-Race, Non-Hispanic"])
+    end
   end
 
   # Take in a postgrad intentions page and a row title.
