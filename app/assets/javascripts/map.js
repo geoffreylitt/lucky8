5ca03558a38594f1f09c8c62e79f928e857878b3
Sagar Jauhari <sagarjauhari@gmail.com>
2015-08-29 09:24:01 -0400

    Fixes #3

--- app/assets/javascripts/map.js
+++
@@ -1,8 +0,0 @@
-$(document).on("page:change", function() {
-  var handler = Gmaps.build('Google');
-  handler.buildMap({ internal: {id: 'map'}}, function(){
-    var markers = handler.addMarkers(gon.geocoded_hash);
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
-  });
-});
--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -2,21 +2,18 @@ class SchoolsController < ApplicationController
   def index
     @schools = School.all
 
-    @schools_with_location = @schools.where.not(latitude: nil)
-
-    gon.geocoded_hash = Gmaps4rails.build_markers(@schools_with_location) do |school, marker|
+    schools_with_location = @schools.where.not(latitude: nil)
+    @geocoded_hash = Gmaps4rails.build_markers(schools_with_location) do |school, marker|
       marker.lat school.latitude
       marker.lng school.longitude
     end
-
   end
 
   def show
     @school = School.find(params[:id])
 
-    @schools_with_location = School.where(id: @school.id).where.not(latitude: nil)
-
-    gon.geocoded_hash = Gmaps4rails.build_markers(@schools_with_location) do |school, marker|
+    schools_with_location = School.where(id: @school.id).where.not(latitude: nil)
+    @geocoded_hash = Gmaps4rails.build_markers(schools_with_location) do |school, marker|
       marker.lat school.latitude
       marker.lng school.longitude
     end
